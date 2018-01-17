989b431eb22f33ebed621c643128888c5deca8f3
terence <terencelomoa@gmail.com>
2018-01-16 19:22:32 -0500

    Expres pathing

---
+++ routes/users.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+// Register
+router.get()
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
+// Require dependencies
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const bodyParser = require('body-parser');
+const cors = require('cors');  //access the server from any domain name
+const passport =  require('passport');
 
+// Connect to Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On database connection error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error ' + err);
+});
+
+// Instantiate express server
 const app = express();
 
+const users = require('./routes/users');
+
+
+/* 
 app.get('/api/customers', (req, res) => {
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
@@ -10,8 +35,27 @@ app.get('/api/customers', (req, res) => {
   ];
 
   res.json(customers);
-});
+}); 
+*/
 
+//PORT Number
 const port = 5000;
 
+//CORS Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users',users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
 app.listen(port, () => `Server running on port ${port}`);
