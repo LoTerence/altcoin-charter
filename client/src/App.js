35fc1d89e49a4512960942c2c4567b21612d591a
terence <terencelomoa@gmail.com>
2018-01-19 19:56:05 -0500

    added require_auth function

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Signup from './_components/auth/signup';
 import Feature from "./_components/feature"; //TODO create feature component
 import RequireAuth from './_components/auth/require_auth';
 
-//   -----  code that checks local storage for a 'user' token. In the case that there is, dipatch an action of type authenticated into reducers
+//   -----  code that checks local storage for a 'user' token. In the case that there is, dispatch an action of type authenticated into reducers
 import { AUTH_USER } from './_store/actions/constants';
 const token = localStorage.getItem('token');
 if(token) {
--- client/src/_components/auth/require_auth.js
+++ client/src/_components/auth/require_auth.js
@@ -1,39 +1,38 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 /*
     When the component is mounting or updating (because of changes to props or 
     state) there is an if statement that checks if the user is authenticated.
-     If the user is not, HOC will make a redirect into ‘/signin’ URL. In 
+     If the user is not, HOC will make a redirect into homepage URL. In 
      any other case, nothing happens and the user can go into current component.
 */
 
 export default function (ComposedComponent) {
     class Authentication extends Component{
-        componentWillMount() {
-            if(!this.props.authenticated) {
-                this.props.history.push('/signin');
+        static contextTypes = {
+            router: React.PropTypes.object
+        }
+    
+        componentWillMount(){
+            if(!this.props.authenticated){
+                this.context.router.push('/')
             }
         }
-
-        componentWillUpdate(nextProps) {
-            if(!nextProps.authenticated) {
-                this.props.history.push('/signin');
+    
+        componentWillUpdate(nextProps){
+            if(!nextProps.authenticated){
+                this.context.router.push('/')
             }
         }
-
-        PropTypes = {
-            router: PropTypes.object,
-        }
-
-        render() {
-            return <ComposedComponent {...this.props} />;
+    
+        render(){
+            return <ComposedComponent {...this.props}/>
         }
     }
 
     function mapStateToProps(state) {
-        return { authenticated: state.signin.authenticated };
+        return { authenticated: state.auth.authenticated };
     }
 
     return connect(mapStateToProps)(Authentication);
