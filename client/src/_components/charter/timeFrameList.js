c2a147edcdfb0afb770f9c997b1f7262c21d7a30
terence <terencelomoa@gmail.com>
2018-01-30 19:39:29 -0500

    The timefram buttons can now change color

--- client/src/_components/charter/timeFrameList.js
+++ client/src/_components/charter/timeFrameList.js
@@ -8,12 +8,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setActiveTimeframe, getHistData } from '../../_store/actions/histData';
 
-//TODO: change colors of buttons when a new active timeframe is selected
+// optional TODO: clean up the render function. The code works but its ugly
 
 export class TimeFrameList extends Component {
   static propTypes = {
     setActiveTimeframe: PropTypes.func.isRequired,
-    activeCoin: PropTypes.object
+    activeCoin: PropTypes.object,
+    activeTimeframe: PropTypes.string.isRequired
   }
 
   handleClickEvent(tf) {
@@ -24,22 +25,102 @@ export class TimeFrameList extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
-        <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
-      </div>
-    )
+    let tfl;
+    switch (this.props.activeTimeframe) {
+      case "1hour":
+        tfl = (
+          <div>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "12hours":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "1day":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "1week":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "1month":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "3months":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      case "1year":
+        tfl = (
+          <div>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1hour")}>1Hr</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"12hours")}>12Hrs</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1day")}>Day</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1week")}>Week</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"1month")}>Month</button>
+            <button className="btn btn-default" onClick={this.handleClickEvent.bind(this,"3months")}>3months</button>
+            <button className="btn btn-success" onClick={this.handleClickEvent.bind(this,"1year")}>Year</button>
+          </div>);
+        break;
+      default:
+          tfl = <p>Error in timeFrameList component switch case</p>;
+    }
+    return tfl;
   }
 }
 
 const mapStateToProps = (state) => ({
-  activeCoin: state.coinList.activeCoin
+  activeCoin: state.coinList.activeCoin,
+  activeTimeframe: state.histData.activeTimeframe
 });
 
 const mapDispatchToProps = (dispatch) => {
