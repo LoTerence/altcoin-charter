0cbdb9d49849280f5a2066d56f3ef3229a67ef6b
terence <terencelomoa@gmail.com>
2018-02-03 01:12:02 -0500

    included config vars

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
+const dbuser = process.env.DB_USER;
+const dbpassword = process.env.DB_PASSWORD;
+const mysecret = proccess.env.MYSECRET;
+
 module.exports = {
-    database: 'mongodb://acc_user:bddqef16@ds257627.mlab.com:57627/altcoin-charter',
-    secret:'mysecret'
+    database: 'mongodb://'+dbuser+':'+dbpassword+'@ds257627.mlab.com:57627/altcoin-charter',
+    secret:mysecret
 }
