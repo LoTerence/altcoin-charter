98ffe2ae27222894c001b6e01750529914a80ea1
Terence <terencelomoa@gmail.com>
2020-10-06 13:46:27 -0700

    authentication and /user/ routes redone

--- config/passport.js
+++ config/passport.js
@@ -1,25 +1,34 @@
+const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/User");
 
-module.exports = function (passport) {
-  const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
-    secretOrKey: process.env.JWT_SECRET_KEY,
-  };
-
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.getUserById(jwt_payload.data._id, (err, user) => {
-        if (err) {
-          return done(err, false);
-        }
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        }
-      });
-    })
-  );
+const opts = {
+  secretOrKey: process.env.JWT_SECRET_KEY,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
 };
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    User.getUserById(jwt_payload.data._id, (err, user) => {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "User id not found" });
+      }
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
---
+++ controllers/users.js
@@ -0,0 +1,150 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// @desc Register a new user
+// @route POST /users/register
+// @access private - only the client can access
+exports.registerUser = async (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+    watchList: [],
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.json({ success: false, message: "Email already registered" });
+    } else {
+      const data = {
+        _id: user._id,
+        email: user.email,
+      };
+      const token = jwt.sign({ data: data }, process.env.JWT_SECRET_KEY, {
+        expiresIn: 604800, //1 week
+      });
+      res.json({
+        success: true,
+        token: "JWT " + token,
+        message: "User registered",
+      });
+    }
+  });
+};
+
+// @desc Log in a new user
+// @route POST /users/authenticate
+// @access private - only the client can access
+exports.authenticateUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  User.getUserByEmail(email, async (err, user) => {
+    if (err) throw err;
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    try {
+      const isMatch = await user.isValidPassword(password);
+      if (isMatch) {
+        const data = {
+          _id: user._id,
+          email: user.email,
+        };
+        const token = jwt.sign({ data: data }, process.env.JWT_SECRET_KEY, {
+          expiresIn: 604800, //1 week
+        });
+
+        res.json({
+          success: true,
+          token: "JWT " + token,
+          message: "User logged in",
+        });
+      } else {
+        return res.json({ success: false, message: "Wrong password" });
+      }
+    } catch (error) {
+      throw error;
+    }
+  });
+};
+
+// @desc Get the user profile
+// @route GET /users/profile
+// @access private - only the client can access
+exports.getUserProfile = async (req, res) => {
+  const { _id, email, watchList } = req.user;
+  res.json({ user: { _id, email, watchList } });
+};
+
+// @desc Get the user's watchlist
+// @route GET /users/watchlist
+// @access private - only the client can access
+exports.getUserWatchlist = async (req, res) => {
+  res.send(req.user.watchList);
+};
+
+// @desc Add a coin to the user's watchlist
+// @route PUT /users/watchlist/addcoin
+// @access private - only the client can access
+exports.addCoinToWatchlist = async (req, res) => {
+  const newCoin = req.body.newCoin;
+  User.getUserById(req.user._id, (err, user) => {
+    //handle errors
+    if (err) throw err;
+    if (!user) {
+      console.log(
+        "error in server/routes/users.js -- router.put('/watchlist/addcoin')"
+      );
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    // Chcek if the coin is already in the watchlist by comparing its symbol
+    if (user.watchList.filter((e) => e.Symbol === newCoin.Symbol).length) {
+      return res.json({
+        success: false,
+        msg: "That coin is already on the list",
+      });
+    }
+
+    // update watchlist with the new coin obj from req.body, then res.json the new watchlist
+    user.watchList.push(newCoin);
+    user.save((err) => {
+      if (err)
+        res.json({
+          success: false,
+          msg:
+            "error saving new coin in server/routes/users.js -- router.put('/watchlist/addcoin')",
+        });
+      res.json({ success: true, newWatchList: user.watchList });
+    });
+  });
+};
+
+// @desc Delete a coin from the user's watchlist
+// @route PUT /users/watchlist/delcoin
+// @access private - only the client can access
+exports.delCoinFromWatchlist = async (req, res) => {
+  User.getUserById(req.user._id, (err, user) => {
+    //handle errors
+    if (err) throw err;
+    if (!user) {
+      console.log(
+        "error in server/routes/users.js -- router.put('/watchlist/addcoin')"
+      );
+      return res.json({ success: false, msg: "User not found" });
+    }
+
+    user.watchList = user.watchList.filter((e) => e.Symbol !== req.body.Symbol);
+
+    user.save((err) => {
+      if (err)
+        res.json({
+          success: false,
+          msg:
+            "error deleting coin in routes/users.js - put(watchlist/delcoin)",
+        });
+      res.json({ success: true, newWatchList: user.watchList });
+    });
+  });
+};
