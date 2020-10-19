935826292d0507be9a80455c00c8dc7125c1841e
Terence <terencelomoa@gmail.com>
2020-10-18 23:08:06 -0700

    Turned footer into functional component

--- client/src/_components/universal/footer.js
+++ client/src/_components/universal/footer.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer container">
-        <p>
-            <a href="https://github.com/LoTerence">@loterence</a>
-        </p>
-        <p>
-            project link: <a href="https://github.com/LoTerence/altcoin-charter">@loterence/altcoin-charter</a>
-        </p>
-        <p>
-            Credit for cryptocoin data goes to: <a href="https://www.cryptocompare.com/api/">cryptocompare.com/api</a>
-        </p>
-      </footer>
-    )
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="footer container">
+      <p>
+        <a href="https://github.com/LoTerence">@loterence</a>
+      </p>
+      <p>
+        project link:{" "}
+        <a href="https://github.com/LoTerence/altcoin-charter">
+          @loterence/altcoin-charter
+        </a>
+      </p>
+      <p>
+        Credit for cryptocoin data goes to:{" "}
+        <a href="https://www.cryptocompare.com/api/">cryptocompare.com/api</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
