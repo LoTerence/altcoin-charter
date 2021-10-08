9eb03a054c1da5276d27ad5d1ab0f679c4b079cf
Terence <terencelomoa@gmail.com>
2021-10-07 17:19:40 -0700

    added search suggestions to new coin search bar, put bandaid on adding same coin bug

--- client/src/_components/charter/CoinAdder.js
+++ client/src/_components/charter/CoinAdder.js
@@ -1,10 +1,10 @@
 // The block in the coinUList component that lets the user add a new AltCoin
 
 //TODO: add custom styling
-//TODO: add loading indicator
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import {
   addCoinAction,
   selectCoinList,
@@ -15,13 +15,43 @@ import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 const CoinAdder = () => {
   const dispatch = useDispatch();
   const [symbol, setSymbol] = useState("");
+  const [symbols, setSymbols] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
   const errorMessage = useSelector(selectCoinList).error;
   const reqInProgress = useSelector(selectCoinList).reqInProgress;
+
+  useEffect(() => {
+    const loadSymbols = async () => {
+      const res = await axios.get(
+        "https://min-api.cryptocompare.com/data/all/coinlist?summary=true"
+      );
+      setSymbols(Object.values(res.data.Data));
+    };
+    loadSymbols();
+  }, []);
+
+  const onChangeHandler = (e) => {
+    setSymbol(e);
+    let matches = [];
+    if (e.length > 0) {
+      matches = symbols.filter((sym) => {
+        const regex = new RegExp(`${e}`, "gi");
+        return sym.Symbol.match(regex);
+      });
+    }
+    setSuggestions(matches);
+  };
+
+  const onSuggestHandler = (text) => {
+    setSymbol(text);
+    setSuggestions([]);
+  };
 
   function handleBtnClick(e) {
     e.preventDefault();
     dispatch(addCoinAction(symbol));
     setSymbol("");
+    setSuggestions([]);
   }
 
   function renderAddButton() {
@@ -48,6 +78,29 @@ const CoinAdder = () => {
     }
   }
 
+  function renderSuggestions() {
+    if (suggestions.length > 0) {
+      let suggestions_s = suggestions.sort((a, b) => {
+        return a.Id - b.Id;
+      });
+      return (
+        <div className="suggestions">
+          {suggestions_s.slice(0, 10).map((suggestion) => {
+            return (
+              <div
+                key={suggestion.Id}
+                className="suggestion"
+                onClick={() => onSuggestHandler(suggestion.Symbol)}
+              >
+                {suggestion.FullName}
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  }
+
   function renderAlert() {
     if (errorMessage) {
       return <div className="alert alert-danger">{errorMessage}</div>;
@@ -66,7 +119,13 @@ const CoinAdder = () => {
             className="form-control form-control-sm"
             placeholder="Altcoin Symbol, i.e. BTC, LTC..."
             value={symbol}
-            onChange={(e) => setSymbol(e.target.value)}
+            onChange={(e) => onChangeHandler(e.target.value)}
+            autoComplete="off"
+            onBlur={() => {
+              setTimeout(() => {
+                setSuggestions([]);
+              }, 100);
+            }}
           />
           <label style={{ opacity: "0.5" }} htmlFor="addNewCoinInput">
             Altcoin Symbol, i.e. BTC, LTC...
@@ -74,7 +133,9 @@ const CoinAdder = () => {
 
           {renderAddButton()}
         </div>
+        {renderSuggestions()}
       </form>
+
       {renderAlert()}
     </div>
   );
