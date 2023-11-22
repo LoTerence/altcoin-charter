19c23cbc2af2001ffa6adb40e42235ac4d102416
Terence Lo <terencelomoa@gmail.com>
2023-11-21 20:42:11 -0800

    footer: links should open to new tab

--- client/src/_components/universal/Footer.js
+++ client/src/_components/universal/Footer.js
@@ -5,17 +5,33 @@ const Footer = () => {
     <footer className="container-fluid mt-5 text-center bg-light">
       <div className="container">
         <p>
-          <a href="https://github.com/LoTerence">@loterence</a>
+          <a
+            href="https://github.com/LoTerence"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @loterence
+          </a>
         </p>
         <p>
           Project link:{" "}
-          <a href="https://github.com/LoTerence/altcoin-charter">
+          <a
+            href="https://github.com/LoTerence/altcoin-charter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             @loterence/altcoin-charter
           </a>
         </p>
         <p>
           Credit for cryptocoin data goes to:{" "}
-          <a href="https://www.cryptocompare.com/api/">cryptocompare.com/api</a>
+          <a
+            href="https://www.cryptocompare.com/api/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            cryptocompare.com/api
+          </a>
         </p>
       </div>
     </footer>
