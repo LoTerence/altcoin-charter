122308814cd6f9b3f1fe3a2d5e2db079d11f15d2
Terence Lo <terencelomoa@gmail.com>
2023-11-22 00:02:40 -0800

    fix scss bug

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Homepage from "./_components/Homepage";
 import { useDispatch } from "react-redux";
 import { authenticate } from "./_store/reducers/authSlice";
+import "./App.scss";
 
 // extract user from token then authenticate
 function App() {
