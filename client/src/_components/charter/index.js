826999f8ee456186bfbf96b466766e68b1543170
Terence Lo <terencelomoa@gmail.com>
2023-11-24 01:20:24 -0800

    refactor Timeframes component

--- client/src/_components/charter/index.js
+++ client/src/_components/charter/index.js
@@ -3,15 +3,13 @@
  * container for the application
  */
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
 import CoinUList from "./CoinUList";
 import PriceChart from "./PriceChart";
 import CoinInfo from "./CoinInfo";
 import TimeFrameList from "./TimeFrameList";
 import CoinNameInfo from "./CoinNameInfo";
-
 import {
   setActiveCoin,
   setTimeFrame,
