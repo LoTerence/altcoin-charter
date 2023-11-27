81ed5d2eefca33538ec4f1c54c9ad747ff82acf2
Terence Lo <terencelomoa@gmail.com>
2023-11-26 16:31:16 -0800

    fetchHistory async think

--- client/src/_store/reducers/historySlice.js
+++ client/src/_store/reducers/historySlice.js
@@ -13,7 +13,7 @@ const initialState = {
   activeTimeframe: "1day",
   coinInfo: null,
   error: null,
-  histData: null,
+  historicalData: null,
   status: "idle",
 };
 
@@ -30,10 +30,6 @@ export const historySlice = createSlice({
     },
     setError: (state, action) => {
       state.error = action.payload;
-    },
-    setHistData: (state, action) => {
-      state.histData = action.payload;
-      state.status = "idle";
     },
     setStatus: (state, action) => {
       state.status = action.payload;
@@ -54,12 +50,22 @@ export const historySlice = createSlice({
       .addCase(fetchCoinInfo.rejected, (state, action) => {
         state.status = "failed";
         state.error = "Error: something went wrong, please try again later 😢";
+      })
+      .addCase(fetchHistory.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchHistory.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.historicalData = action.payload;
+      })
+      .addCase(fetchHistory.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = "Error: something went wrong, please try again later 😢";
       });
   },
 });
 
 export const {
-  setHistData,
   setActiveCoinId,
   setCoinInfo,
   setError,
@@ -68,68 +74,55 @@ export const {
 } = historySlice.actions;
 
 // Async thunks
-// get the historical data from the cryptocompare api and save it to histData
-// TODO: I only need the coin.Name
-export const getHistData = (coin, timeframe) => (dispatch) => {
-  dispatch(setStatus("loading"));
-  let histo;
-  switch (timeframe) {
-    case "1hour":
-      histo = { timeUnit: "histominute", limit: 60 };
-      break;
-    case "12hours":
-      histo = { timeUnit: "histominute", limit: 144 };
-      break;
-    case "1day":
-      histo = { timeUnit: "histominute", limit: 288 };
-      break;
-    case "1week":
-      histo = { timeUnit: "histohour", limit: 168 };
-      break;
-    case "1month":
-      histo = { timeUnit: "histoday", limit: 31 };
-      break;
-    case "3months":
-      histo = { timeUnit: "histoday", limit: 92 };
-      break;
-    case "1year":
-      histo = { timeUnit: "histoday", limit: 365 };
-      break;
-    default:
-      console.log(
-        "error in getHistData function in actions/histData timeframe cases"
-      );
+// get the historical data from the cryptocompare api and save it to historicalData
+export const fetchHistory = createAsyncThunk(
+  "history/fetchHistory",
+  async ({ coinSymbol, timeframe }) => {
+    let histo;
+    switch (timeframe) {
+      case "1hour":
+        histo = { timeUnit: "histominute", limit: 60 };
+        break;
+      case "12hours":
+        histo = { timeUnit: "histominute", limit: 144 };
+        break;
+      case "1day":
+        histo = { timeUnit: "histominute", limit: 288 };
+        break;
+      case "1week":
+        histo = { timeUnit: "histohour", limit: 168 };
+        break;
+      case "1month":
+        histo = { timeUnit: "histoday", limit: 31 };
+        break;
+      case "3months":
+        histo = { timeUnit: "histoday", limit: 92 };
+        break;
+      case "1year":
+        histo = { timeUnit: "histoday", limit: 365 };
+        break;
+      default:
+        console.log("error in fetchHistory function in timeframe cases");
+    }
+    const cryptocompareRes = await axios.get(
+      `https://min-api.cryptocompare.com/data/${histo.timeUnit}?fsym=${coinSymbol}&tsym=USD&limit=${histo.limit}`
+    );
+    const data = cryptocompareRes.data.Data;
+    const historicalData = [];
+    //loop through the "Data" array from the json res and save its time property as the x coordinate and close property as the y coordinate
+    for (let i = 0; i < data.length; i++) {
+      const coord = {
+        time: data[i].time,
+        price: data[i].close,
+      };
+      historicalData.push(coord);
+    }
+    return historicalData;
   }
-
-  axios
-    .get(
-      `https://min-api.cryptocompare.com/data/${histo.timeUnit}?fsym=${coin.Name}&tsym=USD&limit=${histo.limit}`
-    )
-    .then((res) => {
-      //array that will hold the historical data/payload/xy coordinates
-      let histData = [];
-      //loop through the "Data" array from the json res and save its time property as the x coordinate and close property as the y coordinate
-      for (let i = 0; i < res.data.Data.length; i++) {
-        // let date = new Date(res.data.Data[i].time * 1000);
-        let coord = {
-          time: res.data.Data[i].time,
-          price: res.data.Data[i].close,
-        };
-        histData.push(coord);
-      }
-
-      dispatch(setHistData(histData));
-    })
-    .catch((err) => {
-      console.log(
-        "error in getHistData method api call to cryptocompare.com: \n" + err
-      );
-      dispatch(setError("error fetching data from cryptocompare.com"));
-      dispatch(setStatus("idle"));
-    });
-};
+);
 
 // Get the coin data from the cryptocompare api and save it to coinInfo
+// TODO: I only need the coin.Symbol
 export const fetchCoinInfo = createAsyncThunk(
   "history/fetchCoinInfo",
   async (coin) => {
