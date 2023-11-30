412452d53f7d547c5ff2ec935aaeb62267acb1a7
Terence Lo <terencelomoa@gmail.com>
2023-11-30 12:21:43 -0800

    small refactors

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// Require dependencies
-const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
+const express = require("express");
 const session = require("express-session");
 const path = require("path");
 const passport = require("passport");
-const cors = require("cors"); //access the server from any domain name
 const connectDB = require("./config/database");
 
 // allows project to read from .env
@@ -31,12 +30,11 @@ app.use(
   })
 );
 
-// CORS Middleware
+// CORS Middleware: access the server from any domain name
 app.use(cors());
 /* TODO: add option {origin: "https://altcoin-charter.herokuapp.com/" } or whatever the origin that the 
-front end is running on so the server can only accept requests from the front end 
-app.use(cors({origin: "https://altcoin-charter.herokuapp.com/"}));
-*/
+  front end is running on so the server can only accept requests from the front end */
+// app.use(cors({origin: "https://altcoin-charter.herokuapp.com/"}));
 
 // Passport Middleware
 app.use(passport.initialize());
