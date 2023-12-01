8d28e973d77e93c99a3f3bcee0883d2058622e66
Terence Lo <terencelomoa@gmail.com>
2023-12-01 02:44:05 -0800

    small refactors

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ChartTitle from "../_components/charter/ChartTitle";
 import CoinAdder from "../_components/charter/CoinAdder";
+import CoinCard from "../_components/charter/CoinCard";
 import CoinInfo from "../_components/charter/CoinInfo";
-import CoinLi from "../_components/charter/CoinLi";
 import PriceChart from "../_components/charter/PriceChart";
 import TimeFrameList from "../_components/charter/TimeFrameList";
 import {
@@ -39,7 +39,7 @@ const Home = () => {
       {status === "loading" && <p>loading coins..</p>}
       <div className="d-flex flex-wrap">
         {coins.map((coin) => (
-          <CoinLi
+          <CoinCard
             key={coin._id}
             coin={coin}
             deleteCoin={deleteCoin}
