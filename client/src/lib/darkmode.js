8031ffd26f3dbd77b0f52f0b51629e2c1580f4e6
Terence Lo <terencelomoa@gmail.com>
2023-12-05 00:56:49 -0800

    dark mode functionality

---
+++ client/src/lib/darkmode.js
@@ -0,0 +1,7 @@
+export const isDarkModeEnabled = () => {
+  return (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+};
