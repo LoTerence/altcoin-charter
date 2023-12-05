495c941aca916a8edfd194f3cbd57cd91df3512d
Terence Lo <terencelomoa@gmail.com>
2023-12-05 00:18:38 -0800

    theme icons

--- client/src/_components/icons/index.js
+++ client/src/_components/icons/index.js
@@ -1,7 +1,19 @@
+import CircleHalfIcon from "./CircleHalfIcon";
+import DarkIcon from "./DarkIcon";
 import GoogleIcon from "./GoogleIcon";
 import FacebookIcon from "./FacebookIcon";
+import LightIcon from "./LightIcon";
 import ProfileIcon from "./ProfileIcon";
 import TrashIcon from "./TrashIcon";
 import SpinnerIcon from "./SpinnerIcon";
 
-export { GoogleIcon, FacebookIcon, ProfileIcon, TrashIcon, SpinnerIcon };
+export {
+  CircleHalfIcon,
+  DarkIcon,
+  GoogleIcon,
+  FacebookIcon,
+  LightIcon,
+  ProfileIcon,
+  TrashIcon,
+  SpinnerIcon,
+};
