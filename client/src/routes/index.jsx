e1907ec4cafa04bbe4e5e65888f63f0029414af2
Terence Lo <terencelomoa@gmail.com>
2023-12-14 00:54:32 -0800

    lazy load some pages

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,11 +1,12 @@
+import { lazy } from "react";
 import Layout from "./Layout";
-import ContactUs from "../pages/ContactUs";
+const ContactUs = lazy(() => import("../pages/ContactUs"));
 import ErrorPage from "./ErrorPage";
 import Home from "../pages/Home";
-import PrivacyPolicy from "../pages/PrivacyPolicy";
+const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy"));
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
-import OAuthCallback from "../pages/OAuthCallback";
+const OAuthCallback = lazy(() => import("../pages/OAuthCallback"));
 // TODO: lazy load private routes?
 import Feature from "../pages/Feature";
 import Profile from "../pages/Profile";
