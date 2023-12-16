82e6b74389f21f2f3c42bd42b03995a58fca03aa
Terence Lo <terencelomoa@gmail.com>
2023-12-15 20:21:38 -0800

    Change name of Loading component

--- client/src/_components/universal/Loading.jsx
+++ client/src/_components/universal/Loading.jsx
@@ -1,6 +1,6 @@
 import { SpinnerIcon } from "../icons";
 
-const LoadingOverlay = ({ isLoading = true }) => {
+const Loading = ({ isLoading = true }) => {
   return (
     <>
       {isLoading && (
@@ -12,4 +12,4 @@ const LoadingOverlay = ({ isLoading = true }) => {
   );
 };
 
-export default LoadingOverlay;
+export default Loading;
