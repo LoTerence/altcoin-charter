554e6d62378d8cfc1ce6d415aa86abc4eb21b874
Terence Lo <terencelomoa@gmail.com>
2024-03-29 15:44:29 -0700

    update gitignore

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-// const keys = require("../config/keys");
-// const { apiURL } = keys.app;
-
-// const api = `/${apiURL}`;
 const api = `/api`;
 
 // api routes
