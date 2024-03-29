aa1ef62641c5229eb1029a1c1d1314e8cab5e73e
Terence Lo <terencelomoa@gmail.com>
2024-03-29 16:41:44 -0700

    init keys file

---
+++ server/config/keys.js
@@ -0,0 +1,26 @@
+module.exports = {
+  app: {
+    name: "Crypto Charts",
+    apiURL: `${process.env.BASE_API_URL}`,
+    clientURL: process.env.CLIENT_URL,
+    serverURL: process.env.SERVER_URL,
+  },
+  port: process.env.PORT || 5000,
+  database: {
+    url: process.env.MONGO_URI,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    tokenLife: "7d",
+  },
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  facebook: {
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+  },
+};
