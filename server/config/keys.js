9d949d7d46b7991acf54a59631e92dbec38285e4
Terence Lo <terencelomoa@gmail.com>
2024-03-29 16:48:36 -0700

    rm port key

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,7 +5,6 @@ module.exports = {
     clientURL: process.env.CLIENT_URL,
     serverURL: process.env.SERVER_URL,
   },
-  port: process.env.PORT || 5000,
   database: {
     url: process.env.MONGO_URI,
   },
