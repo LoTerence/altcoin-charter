2dadf50098b3fa19c28052f3e2a880709baf9b1c
Terence Lo <terencelomoa@gmail.com>
2024-03-29 18:09:04 -0700

    include ALL env vars

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,12 +5,16 @@ module.exports = {
     clientURL: process.env.CLIENT_URL,
     serverURL: process.env.SERVER_URL,
   },
+  port: process.env.PORT || 5000,
   database: {
     url: process.env.MONGO_URI,
   },
   jwt: {
     secret: process.env.JWT_SECRET,
     tokenLife: "7d",
+  },
+  session: {
+    secret: process.env.SESSION_SECRET,
   },
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
