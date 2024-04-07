9f2956f9bfc20ae5c315f516b54973ac28d0dea2
Terence Lo <terencelomoa@gmail.com>
2024-04-07 12:16:44 -0700

    docker vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "",
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const SERVER_URL = `${
+    process.env.VITE_APP_SERVER_URL ?? "http://localhost:5000"
+  }`;
+
+  return {
+    base: "",
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: `${SERVER_URL}/api`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+      watch: {
+        usePolling: true,
       },
     },
-    watch: {
-      usePolling: true,
-    },
-  },
+  };
 });
