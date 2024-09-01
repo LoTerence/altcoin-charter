140bd66e05d9f8df3203bbc8ec7c8a08623e4093
Terence Lo <terencelomoa@gmail.com>
2024-08-31 22:31:21 -0700

    rm initbodyscript

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from "./_store";
 import { Provider } from "react-redux";
+import { initDarkMode } from "./lib/darkmode";
+
+initDarkMode();
 
 const root = createRoot(document.getElementById("root"));
 
