adcafcef24e81ab0de85991f8296cf210b60db72
Terence Lo <terencelomoa@gmail.com>
2024-09-01 18:52:52 -0700

    init error classes

---
+++ server/utils/error-classes.js
@@ -0,0 +1,26 @@
+const httpStatusCodes = require("./http-status-codes");
+
+class BaseError extends Error {
+  constructor(name, statusCode, isOperational, description) {
+    super(description);
+
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = name;
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    Error.captureStackTrace(this);
+  }
+}
+
+class Api404Error extends BaseError {
+  constructor(
+    name,
+    statusCode = httpStatusCodes.NOT_FOUND,
+    description = "Not found.",
+    isOperational = true
+  ) {
+    super(name, statusCode, isOperational, description);
+  }
+}
+
+module.exports = { BaseError, Api404Error };
