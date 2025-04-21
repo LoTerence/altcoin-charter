62fae93166eb68d0f4814e90287dc61f8d978f84
Terence Lo <terencelomoa@gmail.com>
2025-04-20 18:43:35 -0700

    init vitest

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -14,7 +14,8 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     resolve: {
       alias: {
-        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+        "@": resolve("./src"),
       },
     },
     server: {
